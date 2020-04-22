9ce8787dc46dba1e32d5258191497729a7d2b040
root <root@ip-172-31-23-187.ap-northeast-2.compute.internal>
2020-04-22 16:36:14 +0900

    typos gone

--- chapter_01/1_testing_floats.py
+++ chapter_01/1_testing_floats.py
@@ -7,14 +7,14 @@ def float_to_fractions(number):
     return Fraction(*number.as_integer_ratio())
     
 def get_denominator(number1, number2):
-    "" 분모를 반환한다.""
+    """ 분모를 반환한다."""
     a = Fraction(number1, number2)
     return a.denominator
     
 def get_numerator(number1, number2):
-    "" 분자를 반환한다.""
+    """ 분자를 반환한다."""
     a = Fraction(number1, number2)
-    return a.numberator
+    return a.numerator
     
 def test_testing_floats():
     number1 = 1.25
@@ -23,12 +23,12 @@ def test_testing_floats():
     number4 = 5/4
     number6 = 6
     assert(rounding_floats(number1, number2) == 1.2)
-    asser(rounding_floats(number1*10, number3) == 10)
+    assert(rounding_floats(number1*10, number3) == 10)
     assert(float_to_fractions(number1) == number4)
     assert(get_denominator(number2, number6) == number6)
     assert(get_numerator(number2, number6) == number2)
     print("테스트 통과!")
     
-if __name-- == "__main__":
+if __name__ == "__main__":
     test_testing_floats()
     
