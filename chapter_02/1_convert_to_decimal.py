918ef6428af855533aa0743ae67bf158355d78a1
jaehaahn <jaeha.ahn@gmail.com>
2020-04-23 15:07:33 +0900

    Update 1_convert_to_decimal.py

--- chapter_02/1_convert_to_decimal.py
+++ chapter_02/1_convert_to_decimal.py
@@ -8,7 +8,7 @@ def convert_to_decimal(number, base):
     
 def test_convert_to_decimal():
     number, base = 1001, 2
-    assert(convert_to_decimal(number, base) == 0)
+    assert(convert_to_decimal(number, base) == 9)
     print("테스트 통과")
     
 if __name__ == "__main__":
