ced3d787a9daca9fbdaea0e5aac90c022b8e4082
root <root@ip-172-31-23-187.ap-northeast-2.compute.internal>
2020-05-01 09:28:45 +0900

    ch2 eg1

--- chapter_02/1_count_unique_words.py
+++ chapter_02/1_count_unique_words.py
@@ -5,11 +5,12 @@ def count_unique_word():
     words = {}
     strip = string.whitespace + string.punctuation + string.digits + "\"'"
     for filename in sys.argv[1:]:
-    with open(filename) as file:
-        for line in file:
-            for word = word.strip(strip)
-            if len(word) > 2:
-                words[word] = words.get(word,0) + 1
+        with open(filename) as file:
+            for line in file:
+                for word in line.lower().split():
+                    word = word.strip(strip)
+                    if len(word) > 2:
+                        words[word] = words.get(word,0) + 1
     for word in sorted(words):
         print("{0}: {1}ㅂㅓㄴ".format(word, words[word]))
         
