72e5ceb3d95f23f35fd6f6aa66bbd325803443da
root <root@ip-172-31-23-187.ap-northeast-2.compute.internal>
2020-05-19 23:59:07 +0900

    jtp ch5 module mod2

--- jtp_ch05/ch5module_practice.py
+++ jtp_ch05/ch5module_practice.py
@@ -52,3 +52,19 @@ Type "help", "copyright", "credits" or "license" for more information.
 >>> exit()
 (base) [root@ip-172-31-23-187 jtp_ch05]#
 
+
+
+(base) [root@ip-172-31-23-187 jtp_ch05]# python3
+Python 3.7.6 (default, Jan  8 2020, 19:59:22)
+[GCC 7.3.0] :: Anaconda, Inc. on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import mod2
+>>> print(mod2.PI)
+3.141592
+>>> a = mod2.Math()
+>>> print(a.solv(2))
+12.566368
+>>> print(mod2.add(mod2.PI, 4.4))
+7.5415920000000005
+>>> exit()
+
