4aebeaa4506d750372c7dcb512dc43d626432395
root <root@ip-172-31-23-187.ap-northeast-2.compute.internal>
2020-06-10 23:35:23 +0900

    jtp ch04 input

---
+++ jtp_ch04/jtp_ch04_input_practice.py
@@ -0,0 +1,46 @@
+Last login: Tue Jun  9 13:05:44 2020 from 121.130.250.127
+
+       __|  __|_  )
+       _|  (     /   Amazon Linux 2 AMI
+      ___|\___|___|
+
+https://aws.amazon.com/amazon-linux-2/
+17 package(s) needed for security, out of 34 available
+Run "sudo yum update" to apply all updates.
+[ec2-user@ip-172-31-23-187 ~]$ sudo su
+(base) [root@ip-172-31-23-187 ec2-user]# python3
+Python 3.7.6 (default, Jan  8 2020, 19:59:22) 
+[GCC 7.3.0] :: Anaconda, Inc. on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a = input()
+Life is too short, you need python
+>>> a
+'Life is too short, you need python'
+>>> a = input()
+Yada Yada
+>>> a
+'Yada Yada'
+>>> number = input("숫자를 입력하세요: ")
+숫자를 입력하세요: 1223
+>>> number
+'1223'
+>>> a = 123
+>>> print(a)
+123
+>>> a = "Python"
+>>> print(a)
+Python
+>>> a = [1, 2, 3]
+>>> print(a)
+[1, 2, 3]
+>>> print("life" "is" "too short")
+lifeistoo short
+>>> print("life"+"is"+"too short")
+lifeistoo short
+>>> print("life", "is", "too short")
+life is too short
+>>> for i in range(10):
+...     print(i, end =' ')
+... 
+0 1 2 3 4 5 6 7 8 9 >>> 
+
